93fa00fb86abcfd75fe2ac0ce9ea9ffb3e2873c1
Clara <cfrausto@umail.ucsb.edu>
2019-01-19 21:04:49 -0800

    Scripts folder created

---
+++ UnityZeldaLTTP/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player.cs : MonoBehaviour {
+
+    public float moveSpeed = 5;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+    Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+
+	}
+}
