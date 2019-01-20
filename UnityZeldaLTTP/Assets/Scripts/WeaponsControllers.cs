d2c28f40e33c43a9ace6c0d907fc85e539edec74
Eric Miller <ericdmiller600@gmail.com>
2019-01-19 21:42:00 -0800

    Part of the weapons lul

---
+++ UnityZeldaLTTP/Assets/Scripts/WeaponsControllers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public Transform weaponHold;
+	public Gun startingWeapon;
+	Weapon equippedWeapon;
+	void Start () {
+		if (startingWeapon != null) {
+			EquipWeapon(startingWeapon);
+		}
+		
+	}
+	
+	public void EquipWeapon(Weapon weaponToEquip) {
+		if (equippedWeapon != null) {
+			DestroyWeapon(equippedWeapon.gameObject);
+		}
+		equippedGun = Instantiate (weaponToEquip, weaponHold.position, weaponHold.rotation) as Weapon;
+		equippedWeapon.transform.parent =weaponHold;
+
+	}
+
+}
+
