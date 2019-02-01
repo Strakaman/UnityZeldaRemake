56f6f28a154b0811ca90cee89a914bc21f5aae83
Strakaman <straka3000@gmail.com>
2019-01-31 22:14:26 -0800

    grab audio manager from tutorial and add it to game

---
+++ UnityZeldaLTTP/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public enum AudioChannel {Master, Sfx, Music};
+
+	public float masterVolumePercent { get; private set; }
+	public float sfxVolumePercent { get; private set; }
+	public float musicVolumePercent { get; private set; }
+
+	AudioSource _sfx2DSource;
+	AudioSource[] _musicSources;
+	int _activeMusicSourceIndex;
+
+	public static AudioManager instance;
+
+	Transform _audioListener;
+	Transform _playerT;
+
+	SoundLibrary _library;
+
+	void Awake() {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+
+			_library = GetComponentInChildren<SoundLibrary> ();
+
+			_musicSources = new AudioSource[2];
+			for (int i = 0; i < 2; i++) {
+				GameObject newMusicSource = new GameObject ("Music source " + (i + 1));
+				_musicSources [i] = newMusicSource.AddComponent<AudioSource> ();
+				newMusicSource.transform.parent = transform;
+			}
+			GameObject newSfx2Dsource = new GameObject ("2D sfx source");
+			_sfx2DSource = newSfx2Dsource.AddComponent<AudioSource> ();
+			newSfx2Dsource.transform.parent = transform;
+
+            _audioListener = GetComponentInChildren<AudioListener>().transform;
+			if (FindObjectOfType<Player> () != null) {
+				_playerT = FindObjectOfType<Player> ().transform;
+			}
+
+			masterVolumePercent = PlayerPrefs.GetFloat ("master vol", 1);
+			sfxVolumePercent = PlayerPrefs.GetFloat ("sfx vol", 1);
+			musicVolumePercent = PlayerPrefs.GetFloat ("music vol", 1);
+		}
+	}
+
+	void OnLevelWasLoaded(int index) {
+		if (_playerT == null) {
+			if (FindObjectOfType<Player> () != null) {
+				_playerT = FindObjectOfType<Player> ().transform;
+			}
+		}
+	}
+
+	void Update() {
+		if (_playerT != null) {
+			_audioListener.position = _playerT.position;
+		}
+	}
+
+	public void SetVolume(float volumePercent, AudioChannel channel) {
+		switch (channel) {
+		case AudioChannel.Master:
+			masterVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Sfx:
+			sfxVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Music:
+			musicVolumePercent = volumePercent;
+			break;
+		}
+
+		_musicSources [0].volume = musicVolumePercent * masterVolumePercent;
+		_musicSources [1].volume = musicVolumePercent * masterVolumePercent;
+
+		PlayerPrefs.SetFloat ("master vol", masterVolumePercent);
+		PlayerPrefs.SetFloat ("sfx vol", sfxVolumePercent);
+		PlayerPrefs.SetFloat ("music vol", musicVolumePercent);
+		PlayerPrefs.Save ();
+	}
+
+	public void PlayMusic(AudioClip clip, float fadeDuration = 1) {
+		_activeMusicSourceIndex = 1 - _activeMusicSourceIndex;
+		_musicSources [_activeMusicSourceIndex].clip = clip;
+		_musicSources [_activeMusicSourceIndex].Play ();
+
+		StartCoroutine(AnimateMusicCrossfade(fadeDuration));
+	}
+
+	public void PlaySound(AudioClip clip, Vector3 pos) {
+		if (clip != null) {
+			AudioSource.PlayClipAtPoint (clip, pos, sfxVolumePercent * masterVolumePercent);
+		}
+	}
+
+	public void PlaySound(SoundClipType soundtype, Vector3 pos) {
+		PlaySound (_library.GetClipFromName (soundtype), pos);
+	}
+
+	public void PlaySound2D(SoundClipType soundtype) {
+		_sfx2DSource.PlayOneShot (_library.GetClipFromName (soundtype), sfxVolumePercent * masterVolumePercent);
+	}
+
+	IEnumerator AnimateMusicCrossfade(float duration) {
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * 1 / duration;
+			_musicSources [_activeMusicSourceIndex].volume = Mathf.Lerp (0, musicVolumePercent * masterVolumePercent, percent);
+			_musicSources [1-_activeMusicSourceIndex].volume = Mathf.Lerp (musicVolumePercent * masterVolumePercent, 0, percent);
+			yield return null;
+		}
+	}
+}
---
+++ UnityZeldaLTTP/Assets/Scripts/SoundLibrary.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundLibrary : MonoBehaviour {
+
+	public SoundGroup[] soundGroups;
+
+    Dictionary<SoundClipType, AudioClip[]> groupDictionary = new Dictionary<SoundClipType, AudioClip[]>();
+
+	void Awake() {
+		foreach (SoundGroup soundGroup in soundGroups) {
+			groupDictionary.Add (soundGroup.soundType, soundGroup.group);
+		}
+	}
+
+	public AudioClip GetClipFromName(SoundClipType theSoundType) {
+        if (groupDictionary.ContainsKey(theSoundType))
+        {
+            AudioClip[] sounds = groupDictionary[theSoundType];
+			return sounds [Random.Range (0, sounds.Length)];
+		}
+		return null;
+	}
+
+	[System.Serializable]
+	public class SoundGroup {
+		public SoundClipType soundType;
+		public AudioClip[] group;
+	}
+
+}
