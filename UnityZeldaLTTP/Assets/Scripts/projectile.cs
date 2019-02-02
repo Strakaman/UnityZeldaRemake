0ae1d8f0560f1b0f0a41fccf8927b7b770ba8bf7
Eric Miller <ericdmiller600@gmail.com>
2019-02-02 11:21:35 -0800

    Some more work

---
+++ UnityZeldaLTTP/Assets/Scripts/projectile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	float speed = 10;
+
+	public void setSpeed (float newSpeed ) {
+		speed = newSpeed;
+	}
+	
+	
+	void Update () {
+		transform.Translate(Vector3.forward = Time.deltaTime * speed);
+	}
+}
